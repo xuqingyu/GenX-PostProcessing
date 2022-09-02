6ea21d78748d0df29c4b8aeb2fa1fc0e11ab383c
Qingyu Xu <qingyux@della8.princeton.edu>
2022-09-02 16:23:33 -0400

    changed the filaname in genx_post_processing.R

--- code/genx_post_processing.R
+++ code/genx_post_processing.R
@@ -4,8 +4,8 @@ options(readr.show_progress = FALSE)
 # the naming convention of PG is
 # '[...]/[Running_folder]/[year]/[case_id]_[year]_[case_description]/Results/'
 # so it is in fact preferable to feed in the setup folder;
-RunFdr <- "/Users/qingyuxu/Documents/pjm_ce_all/"
-# RunFdr <- "/tigress/qingyux/GenX/PJM/2022_PJM_CE/pjm_ce_all/"
+# RunFdr <- "/Users/qingyuxu/Documents/pjm_ce_all/"
+RunFdr <- "/tigress/qingyux/GenX/PJM/2022_PJM_CE/pjm_ce_ira_all/"
 settingfile <- 'postprocessing_inputs.csv';
 
 source('./code/Header.R')
